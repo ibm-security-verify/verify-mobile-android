5c7fcfca4a1b71ec8f9571ebeee2ce06b45533dd
Carsten Hagemann <chageman@au1.ibm.com>
2024-02-09 14:36:21 +1000

    FIDO2 SDK sample app

---
+++ sdk/fido2demo/src/main/java/com/ibm/security/verifysdk/fido2/demoapp/NetworkHelper.kt
@@ -0,0 +1,34 @@
+package com.ibm.security.verifysdk.fido2.demoapp
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object NetworkHelper {
+
+    var client = HttpClient(engineFactory = CIO) {
+        install(Logging) {
+            logger = Logger.ANDROID
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        install(HttpTimeout) {
+            connectTimeoutMillis = 15000L
+            requestTimeoutMillis = 15000L
+        }
+    }
+
+    val getInstance = client
+}
---
+++ sdk/fido2demo/src/main/java/com/ibm/security/verifysdk/fido2/demoapp/model/IvCreds.kt
@@ -0,0 +1,11 @@
+package com.ibm.security.verifysdk.fido2.demoapp.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class IvCreds(
+    val name: String = "ivcreds-name",
+    val username: String = "ivcreds-username",
+    val email: String = "ivcreds-email",
+    val AZN_CRED_PRINCIPAL_NAME: String
+)
