1bfe5c898e55716b5b60563a288fb741918875c2
Carsten Hagemann <chageman@au1.ibm.com>
2024-02-20 14:14:59 +1000

    Add classpath for AppScan plugin

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -20,13 +20,14 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.51.0")
+        classpath("gradle.plugin.com.hcl.security:appscan-gradle-plugin:1.0.9")
         classpath("org.jetbrains.dokka:dokka-base:$kotlinVersion")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("org.owasp:dependency-check-gradle:9.0.9")
         classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.51.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
