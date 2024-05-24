f67f9fc09c581a68f5e62c79b349459830bc6fa3
Carsten Hagemann <chageman@au1.ibm.com>
2024-05-24 15:05:04 +1000

    Remove AppScan, Update Kotlin version

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -5,7 +5,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 
-    val kotlinVersion by extra { "1.9.10" }
+    val kotlinVersion by extra { "1.9.20" }
 
     repositories {
         google()
@@ -21,7 +21,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.51.0")
-        classpath("gradle.plugin.com.hcl.security:appscan-gradle-plugin:1.0.9")
         classpath("org.jetbrains.dokka:dokka-base:$kotlinVersion")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
