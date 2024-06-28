df2755f3480876f2381fdc68b24862d71e031003
Carsten Hagemann <chageman@au1.ibm.com>
2024-06-28 23:47:59 +1000

    Update FIDO2 SDK demo

--- sdk/examples/fido2_demo/build.gradle.kts
+++ sdk/examples/fido2_demo/build.gradle.kts
@@ -43,15 +43,15 @@ android {
 
 dependencies {
 
-    val ktorVersion by extra { "2.3.11" }
+    val ktorVersion by extra { "2.3.12" }
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation(project(":core"))
     implementation(project(":fido2"))
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-android:$ktorVersion")
@@ -68,6 +68,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
 }
