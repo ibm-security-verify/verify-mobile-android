c93958be08ccb2070bfe3606458eeebdbcaa7737
Carsten Hagemann <chageman@au1.ibm.com>
2025-04-14 17:28:50 +1000

    Update Core SDK

--- sdk/core/src/main/java/com/ibm/security/verifysdk/core/extension/JsonExt.kt
+++ sdk/core/src/main/java/com/ibm/security/verifysdk/core/extension/JsonExt.kt
@@ -4,11 +4,15 @@
 
 package com.ibm.security.verifysdk.core.extension
 
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 
 fun Any?.toJsonElement(): JsonElement = when (this) {
     is Array<*> -> this.toJsonArray()
@@ -25,3 +29,21 @@ fun Array<*>.toJsonArray() = JsonArray(map { it.toJsonElement() })
 fun Iterable<*>.toJsonArray() = JsonArray(map { it.toJsonElement() })
 fun Map<*, *>.toJsonObject(): JsonObject =
     JsonObject(mapKeys { it.key.toString() }.mapValues { it.value.toJsonElement() })
+
+fun JsonObject.getJsonArrayOrNull(path: String): List<JsonElement>? {
+    return this[path]?.jsonArray?.toList()
+}
+
+fun JsonObject.getStringOrNull(path: String): String? {
+    return this[path]?.jsonPrimitive?.content
+}
+
+fun JsonObject.getStringOrThrow(key: String): String {
+    return this[key]?.jsonPrimitive?.content ?: throw SerializationException("Missing $key")
+}
+
+fun JsonObject.getStringList(key: String): List<String> {
+    return this[key]?.jsonArray?.mapNotNull { it.jsonPrimitive.contentOrNull } ?: emptyList()
+}
+
+
