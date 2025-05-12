88ac0db64651aa6a4d403962b4b46c31d466a92d
Carsten Hagemann <57122841+chagemann@users.noreply.github.com>
2025-05-12 15:36:29 +1000

    Add root level gradle files for Jitpack (#39)
    * Update publication settings
    
    * Fixes versionName issue
    
    * Updates local maven publications paths
    
    * Adds root level gradle files for Jitpack

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+// Root settings.gradle.kts
+rootProject.name = "IBM Security Verify SDK"
+
+include(":adaptive")
+include(":authentication")
+include(":core")
+include(":dc")
+include(":fido2")
+include(":mfa")
